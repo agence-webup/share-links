317803dd3a0c033ded13b67bfb74e6b24a03ea98
Robin Parisi <parisi.robin@gmail.com>
2016-04-13 18:08:57 +0200

    test file

--- tests/test.php
+++ tests/test.php
@@ -1,10 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
+require_once '../src/ShareLinks.php';
 use Webup\ShareLinks\ShareLinks;
 
-echo ShareLinks::twitter('http://example.com');
+echo ShareLinks::twitter('http://example.com', 'text to share', 'hashtag1,hashtag2');
+echo "\n";
 echo ShareLinks::facebook('http://example.com');
+echo "\n";
 echo ShareLinks::googleplus('http://example.com');
-echo ShareLinks::linkedin('http://example.com');
+echo "\n";
+echo ShareLinks::linkedin('http://example.com', 'title', 'summary', 'source');
+echo "\n";
+echo ShareLinks::email('name@example.com', 'subject', 'body');
+echo "\n";
