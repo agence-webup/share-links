c09d955ccaac5fa565dcbdb89da2841b267c1d25
Robin Parisi <parisi.robin@gmail.com>
2016-04-13 18:30:29 +0200

    do some tests

--- tests/ShareLinksTest.php
+++ tests/ShareLinksTest.php
@@ -5,14 +5,28 @@ use Webup\ShareLinks\ShareLinks;
 class ShareLinksTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testTwitterLink()
+    public function testTwitterShareLink()
     {
-        $this->assertEquals('https://twitter.com/share?url=http://example.com', ShareLinks::twitter('http://example.com'));
+        $this->assertEquals('https://twitter.com/share?url=http%3A%2F%2Fexample.com&text=text+to+share&hashtags=hashtag1%2Chashtag2', ShareLinks::twitter('http://example.com', 'text to share', 'hashtag1,hashtag2'));
     }
 
-    public function testTwitterLinkNoParams()
+    public function testFacebookShareLink()
     {
-        $this->assertEquals('https://twitter.com/share', ShareLinks::twitter());
+        $this->assertEquals('http://facebook.com/sharer.php?u=http%3A%2F%2Fexample.com', ShareLinks::facebook('http://example.com'));
     }
 
+    public function testGoogleShareLink()
+    {
+        $this->assertEquals('https://plus.google.com/share?url=http%3A%2F%2Fexample.com', ShareLinks::googleplus('http://example.com'));
+    }
+
+    public function testLinkedinShareLink()
+    {
+        $this->assertEquals('https://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fexample.com&title=title&summary=summary&source=source', ShareLinks::linkedin('http://example.com', 'title', 'summary', 'source'));
+    }
+
+    public function testMailShareLink()
+    {
+        $this->assertEquals('mailto:name@example.com?subject=subject&body=body', ShareLinks::email('name@example.com', 'subject', 'body'));
+    }
 }
